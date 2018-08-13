d2a2be0bddd089f1d45aca4850a0e4128db1fe1c
David <david.hale7@gmail.com>
2018-08-13 14:44:21 -0700

    add form validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
-$("#screenCheck").click(function () {
+$("#screenCheck").click(function() {
   if ($('input[name="screenCheck"]').is(':checked')) {
     $("#hidden").show(1000);
   } else {
     $("#hidden").hide(1000);
   }
 });
+
+$('#submit-estimate').click(function(e) {
+  e.preventDefault();
+  if ($('#windows').val()) {
+    let x = parseInt($('#windows').val());
+    if (isNaN(x)) {
+      alert('Please input a numerical value.')
+    } else {
+      estimateForm(x);
+    }
+  } else {
+    alert('Please fill out all form fields.');
+  }
+});
+
+
+function estimateForm(num) {
+  alert(num);
+}
